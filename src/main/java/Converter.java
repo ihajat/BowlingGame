16a049a49bf1db260cca4ec4f618b1bf314d4225
ihajat <ihajat@hotmail.com>
2019-03-14 16:56:31 +0000

    write code to pass failing test

--- src/main/java/Converter.java
+++ src/main/java/Converter.java
@@ -1,5 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Converter {
 
-
+    public List<Integer> convert(String f){
+        List<Integer> rolls = new ArrayList<>();
+        String[] frames = f.split(" ");
+        for (String frame : frames) {
+            String[] balls = frame.split("");
+            for (String ball : balls) {
+                rolls.add(Integer.valueOf(ball));
+            }
+        }
+        return rolls;
+    }
 }
