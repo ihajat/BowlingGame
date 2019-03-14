09e01a60b4c7d1e31d3cf15689d9d19202945e01
ihajat <ihajat@hotmail.com>
2019-03-14 14:45:56 +0000

    refactor code

--- src/main/kotlin/BowlingGame.kt
+++ src/main/kotlin/BowlingGame.kt
@@ -1,6 +1,8 @@
+import Constants.Companion.MAX_GAME_SIZE
+
 class BowlingGame {
 
-    private var rolls = IntArray(21)
+    private var rolls = IntArray(MAX_GAME_SIZE)
     val score: Int
         get() {
             var score = 0
@@ -20,27 +22,36 @@ class BowlingGame {
             return score
         }
 
+    // Setter used to pass an array of ints to represent a game, rather than keep
+    // a cursor for current frame
     fun setRolls(rolls: IntArray) {
         this.rolls = rolls
     }
 
+    //Scorer for standard scores( non spares/strikes )
     private fun getStandardScore(rollIndex: Int): Int {
         return rolls[rollIndex] + rolls[rollIndex + 1]
     }
 
-    private fun isSpare(rollIndex: Int): Boolean {
-        return getStandardScore(rollIndex) == 10
-    }
-
+    //Scorer for standard Spare
     private fun getSpareScore(rollIndex: Int): Int {
         return rolls[rollIndex] + rolls[rollIndex + 1] + rolls[rollIndex + 2]
     }
 
+    //Scorer for standard Strike
+    private fun getStrikeScore(i: Int): Int {
+        return rolls[i] + rolls[i + 1] + rolls[i + 2]
+    }
+
+    // Check if spare
+    private fun isSpare(rollIndex: Int): Boolean {
+        return getStandardScore(rollIndex) == 10
+    }
+
+    // Check if strike
     private fun isStrike(i: Int): Boolean {
         return rolls[i] == 10
     }
 
-    private fun getStrikeScore(i: Int): Int {
-        return rolls[i] + rolls[i + 1] + rolls[i + 2]
-    }
+
 }
---
+++ src/main/kotlin/Constants.kt
@@ -0,0 +1,5 @@
+class Constants {
+    companion object {
+        const val MAX_GAME_SIZE = 21
+    }
+}
