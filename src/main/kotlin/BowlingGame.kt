621d94a88555ed0f1f83ecdfe3f917b17fb534b6
ihajat <ihajat@hotmail.com>
2019-03-14 14:15:59 +0000

    write code to pass last failing test

--- src/main/kotlin/BowlingGame.kt
+++ src/main/kotlin/BowlingGame.kt
@@ -2,10 +2,15 @@ class BowlingGame {
 
     private var rolls = IntArray(21)
     val score: Int
-    get() {
-        var score = 0
-        return score
-    }
+        get() {
+            var score = 0
+            var rollIndex = 0
+            for (i in 0..9) {
+                score += rolls[i] + rolls[i + 1]
+                rollIndex += 2
+            }
+            return score
+        }
 
     fun setRolls(rolls: IntArray) {
         this.rolls = rolls
