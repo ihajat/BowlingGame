3e3874074b5db28b9f6ed6629d955c3141130234
ihajat <ihajat@hotmail.com>
2019-03-14 14:06:31 +0000

    write code to pass last failing test

--- src/main/kotlin/BowlingGame.kt
+++ src/main/kotlin/BowlingGame.kt
@@ -1,7 +1,13 @@
 class BowlingGame {
+
+    private var rolls = IntArray(21)
     val score: Int
     get() {
         var score = 0
         return score
+    }
+
+    fun setRolls(rolls: IntArray) {
+        this.rolls = rolls
     }
 }
